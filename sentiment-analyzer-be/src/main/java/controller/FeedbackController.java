3f1fa6bf9156826db199d5305bbea421bd374985
Devi Sireesha Seru <114307539+devisireesha@users.noreply.github.com>
2026-06-27 19:58:32 +0530

    Add files via upload

---
+++ sentiment-analyzer-be/src/main/java/controller/FeedbackController.java
@@ -0,0 +1,33 @@
+package controller;
+
+import org.springframework.web.bind.annotation.*;
+import repository.FeedbackRepository;
+import entity.Feedback;
+import service.SentimentAnalysisService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/feedback")
+@CrossOrigin(origins = "http://localhost:5173/")
+public class FeedbackController {
+
+    private final FeedbackRepository feedbackRepository;
+    private final SentimentAnalysisService service;
+
+    public FeedbackController(FeedbackRepository feedbackRepository, SentimentAnalysisService service) {
+        this.feedbackRepository = feedbackRepository;
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Feedback> getAllFeedback() {
+        return feedbackRepository.findAll();
+    }
+
+    @PostMapping
+    public Feedback saveFeedback(@RequestBody String content) {
+        Feedback feedback = service.analyzeFeedback(content);
+        return feedbackRepository.save(feedback);
+    }
+}
---
+++ sentiment-analyzer-be/src/main/java/repository/FeedbackRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+import entity.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+
+}
